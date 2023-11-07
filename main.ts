90fd7d4dcf682a1c5092cb134bece324c4b7d42e
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-07 13:00:25 -0500

    ety

--- main.ts
+++ main.ts
@@ -19,34 +19,34 @@ basic.clearScreen()
 
 // when button A is pressed, leds move downwards from top left corner to bottom right corner
 input.onButtonPressed(Button.A, function () {
-    // setup
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite(0, 0)
+  // setup
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
 
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter + 1
-        basic.pause(500)
-    }
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
+  while (loopCounter <= 5) {
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter = loopCounter + 1
+    basic.pause(500)
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
 })
 
 // when button A is pressed, leds move upwards from bottom right corner to top left corner
 input.onButtonPressed(Button.B, function () {
-    // setup
-    basic.clearScreen()
-    loopCounter = 5
-    sprite = game.createSprite(5, 5)
+  // setup
+  basic.clearScreen()
+  loopCounter = 5
+  sprite = game.createSprite(5, 5)
 
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(500)
+  while (loopCounter >= 0) {
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter = loopCounter - 1
+    basic.pause(500)
     }
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
 })
