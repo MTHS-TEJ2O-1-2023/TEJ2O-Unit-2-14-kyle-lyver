ce0bc9c5b93ce97f9732b5fcf5e6a31ac9fac8d9
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-13 18:20:21 +0000

    pythony

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,42 @@
-"""
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
-"""
-
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variables
+loop_counter = 0
+
+# setup
+display.clear()
+display.show(Image.HAPPY)
+sleep(000)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # setup
+        display.clear()
+        loop_counter = 0
+        # loop
+        while loop_counter < 5:
+            sleep(500)
+            # when button A is pressed, leds move downwards from top left corner to bottom right corner
+            display.set_pixel(loop_counter, loop_counter, 9)
+            sleep(500)
+            display.clear()
+            # loop_counter goes up by 1
+            loop_counter += 1
+        display.show(Image.HAPPY)
+
+    if button_b.is_pressed():
+        # setup
+        display.clear()
+        loop_counter = 4
+        # loop
+        while loop_counter > -1:
+            sleep(500)
+            # when button A is pressed, leds move downwards from bottom right corner to top left corner
+            display.set_pixel(loop_counter, loop_counter, 9)
+            sleep(500)
+            display.clear()
+            # loop_counter goes down by 1
+            loop_counter -= 1
+        display.show(Image.HAPPY)
